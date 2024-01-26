00353951ee8c8baca736c48d17bb672f85441a6a
Omar <olr7742@nyu.edu>
2024-01-26 12:58:49 -0500

    update

--- Deep-Residual-Learning-for-Image-Recognition/train_evaluate.py
+++ Deep-Residual-Learning-for-Image-Recognition/train_evaluate.py
@@ -14,10 +14,9 @@ def evaluate(model,data_loader,device):
       total += len(inputs)
       correct += (torch.max(outputs,1)[1] == labels).sum().item()
     error = (total-correct)/total
-    print(f'Error: {error}%')
   return error
 
-def train(model, epochs, train_loader, test_loader, criterion, optimizer, RESULTS_PATH, MODEL_PATH):
+def train(model, epochs, train_loader, test_loader, criterion, optimizer, RESULTS_PATH, scheduler=None, MODEL_PATH):
   device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
   model.to(device)
   model.train()
@@ -25,7 +24,6 @@ def train(model, epochs, train_loader, test_loader, criterion, optimizer, RESULT
   cols       = ['epoch', 'train_loss', 'train_err', 'test_err']
   results_df = pd.DataFrame(columns=cols).set_index('epoch')
   best_test_err = 1.0
-  print('Epoch \tBatch \tNLLLoss_Train')
   for epoch in range(epochs):
     model.train()
     for inputs,labels, in train_loader:
@@ -36,11 +34,12 @@ def train(model, epochs, train_loader, test_loader, criterion, optimizer, RESULT
       loss = criterion(outputs,labels)
       loss.backward()
       optimizer.step()
-      running_loss += loss.item()
+    if scheduler:
+      scheduler.step()
     model.eval()
     train_err = evaluate(model,train_loader,device)
     test_err  = evaluate(model,test_loader,device)
-    results_df.loc[epoch] = [running_loss/len(train_loader), train_err, test_err]
+    results_df.loc[epoch] = [loss.item(), train_err, test_err]
     results_df.to_csv(RESULTS_PATH)
     print(f'train_err: {train_err} test_err: {test_err}')
     if MODEL_PATH and (test_err < best_test_err):
